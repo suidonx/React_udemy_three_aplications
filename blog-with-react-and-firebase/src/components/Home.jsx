4f8b32840abc035449a2829cd4b447b94d5a719f
suidonx <neesama178@gmail.com>
2026-02-24 16:16:44 +0900

    Firebaseからデータを取得し、記事として表示機能を追加

--- blog-with-react-and-firebase/src/components/Home.jsx
+++ blog-with-react-and-firebase/src/components/Home.jsx
@@ -1,7 +1,38 @@
+import { useEffect, useState } from "react";
+import { db } from "../firebase";
+import "./Home.css";
+import { collection, getDocs } from "firebase/firestore";
+
 export const Home = () => {
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(db, "posts"));
+      setPostList(
+        data.docs.map((doc) => {
+          return { ...doc.data(), id: doc.id };
+        }),
+      );
+    };
+    getPosts();
+  }, []);
   return (
-    <>
-      <h1>Homeです</h1>
-    </>
+    <div className="homePage">
+      {postList.map((post) => {
+        return (
+          <div className="homePageContainer" key={post.id}>
+            <div className="postHeader">
+              <h1>{post.title}</h1>
+            </div>
+            <div className="postTextContainer">{post.post}</div>
+            <div className="postFooter">
+              <h3>@{post.author.username}</h3>
+              <button>削除</button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
   );
 };
